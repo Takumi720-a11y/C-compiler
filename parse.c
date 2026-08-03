5c766d310d4bfb9f44ce5dccc8766eb2e75cacbd
Takumi Shingu <cpjfp28729@yahoo.co.jp>
2026-08-03 15:01:54 +0900

    パーサとトークナイザの機能を分けました

--- parse.c
+++ parse.c
@@ -1,136 +1,18 @@
 //パーサ
 #include "9cc.h"
 
-char *user_input;
-Token *token;
 Node *code[100];
-
-// 現在着目しているトークン
-
-void error(char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
-
-void error_at(char *loc, char *fmt, ...) {
-  va_list ap;
-  va_start(ap, fmt);
-
-  int pos = loc - user_input;
-  fprintf(stderr, "%s\n", user_input);
-  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
-  fprintf(stderr, "^ ");
-  vfprintf(stderr, fmt, ap);
-  fprintf(stderr, "\n");
-  exit(1);
-}
+LVar *locals;
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
-bool consume(char *op) {
-  if (token->kind != TK_RESERVED || 
-      strlen(op) != token->len ||
-      memcmp(token->str, op, token->len))
-    return false;
-  token = token->next;
-  return true;
-}
 
-// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
-// それ以外の場合にはエラーを報告する。
-void expect(char *op) {
-  if (token->kind != TK_RESERVED || 
-      strlen(op) != token->len ||
-      memcmp(token->str , op, token->len))
-    error_at(token->str,"'%c'ではありません", op);
-  token = token->next;
-}
-
-// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
-// それ以外の場合にはエラーを報告する。
-int expect_number() {
-  if (token->kind != TK_NUM)
-    error_at(token->str,"数ではありません");
-  int val = token->val;
-  token = token->next;
-  return val;
-}
-
-bool at_eof() {
-  return token->kind == TK_EOF;
-}
-
-bool startswith(char *p, char *q){
-  return memcmp(p,q,strlen(q)) == 0;
-}
-
-Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
-  Token *tok = calloc(1, sizeof(Token));
-  tok->kind = kind;
-  tok->str = str;
-  tok->len = len;
-  cur->next = tok;
-  return tok;
-}
-
-// 入力文字列pをトークナイズしてそれを返す
-Token *tokenize() {
-  char *p = user_input;
-  Token head;
-  head.next = NULL;
-  Token *cur = &head;
-
-  while (*p) {
-    // 空白文字をスキップ
-    if (isspace(*p)) {
-      p++;
-      continue;
-    }
-    
-    if(startswith(p,"==") || startswith(p,"!=") ||
-       startswith(p,"<=") || startswith(p,">=")){
-      cur = new_token(TK_RESERVED, cur, p,2);
-      p += 2;
-      continue;
-    }
-
-    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '>' || *p == '<' || *p == '=' || *p == ';') {
-      cur = new_token(TK_RESERVED, cur, p,1);
-      p += 1;
-      continue;
-    }
-
-    if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p,0);
-      char *q = p;
-      cur->val = strtol(p, &p, 10);
-      cur->len = p-q;
-      continue;
-    }
-
-    if('a' <= *p && *p <= 'z'){
-      cur = new_token(TK_IDENT,cur,p,1);
-      cur->len = 1;
-      p += 1;
-      continue;
-    }
-
-    error_at(p,"数ではありません");
-  }
-
-  new_token(TK_EOF, cur, p,0);
-  return head.next;
-}
-
-Token *consume_ident(){
-    if(token->kind != TK_IDENT)
-        return NULL;
-    Token *tok = token;
-    token = token->next;
-    return tok;
+LVar *find_lvar(Token *tok){
+	for(LVar *var = locals; var; var = var->next){
+		if(var->len == tok->len && !memcmp(tok->str,var->name,var->len))
+			return var;
+	}
+	return NULL;
 }
 
 Node *new_node(NodeKind kind,Node *lhs, Node *rhs){
@@ -148,7 +30,7 @@ Node *new_node_num(int val){
   return node;
 }
      
-Node *stmt(void);         // = expr";"
+Node *stmt(void);         // = expr";" | "return"expr";"
 Node *expr(void);         // = assign
 Node *assign(void);       // = equality("=" assign)?
 Node *equalty(void);      // = relational("==" relational | "!=" relational)*
@@ -166,9 +48,17 @@ void program() {
 }// = stmt*
 
 Node *stmt(void){
-  Node *node = expr();
-  expect(";");
-  return node;
+  Node *node;
+  if(consume("return")){
+		node = calloc(1,sizeof(Node));
+		node->kind = ND_RETURN;
+		node->lhs = expr();
+	}else{
+		node = expr();
+	}
+	if(!consume(";"))
+		error_at(token->str,"';'ではないトークンです");
+	return node;
 }
 
 Node *expr(void){
@@ -257,7 +147,18 @@ Node *primary(void){
   if(tok){
     Node *node = calloc(1,sizeof(Node));
     node->kind = ND_LVAR;
-    node->offset = (tok->str[0] - 'a' + 1) *8;
+		LVar *lvar = find_lvar(tok);
+		if(lvar){
+			node->offset = lvar->offset;
+		}else{
+			lvar = calloc(1,sizeof(LVar));
+			lvar->next = locals;
+			lvar->name = tok->str;
+			lvar->len = tok->len;
+			lvar->offset = locals->offset+8;
+			node->offset = lvar->offset;
+			locals = lvar;	
+		}
     return node;
   }
   return new_node_num(expect_number()); 
---
+++ tokenizer.c
@@ -0,0 +1,120 @@
+//トークナイザ
+
+#include "9cc.h"
+
+Token *token;
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED || 
+      strlen(op) != token->len ||
+      memcmp(token->str, op, token->len))
+    return false;
+  token = token->next;
+  return true;
+}
+
+// 次のトークンが期待している記号のときには、トークンを1つ読み進める。
+// それ以外の場合にはエラーを報告する。
+void expect(char *op) {
+  if (token->kind != TK_RESERVED || 
+      strlen(op) != token->len ||
+      memcmp(token->str , op, token->len))
+    error_at(token->str,"'%c'ではありません", op);
+  token = token->next;
+}
+
+// 次のトークンが数値の場合、トークンを1つ読み進めてその数値を返す。
+// それ以外の場合にはエラーを報告する。
+int expect_number() {
+  if (token->kind != TK_NUM)
+    error_at(token->str,"数ではありません");
+  int val = token->val;
+  token = token->next;
+  return val;
+}
+
+bool at_eof() {
+  return token->kind == TK_EOF;
+}
+
+bool startswith(char *p, char *q){
+  return memcmp(p,q,strlen(q)) == 0;
+}
+
+int is_alnum(char c){
+    return ('a' <= c && c <= 'z') ||
+           ('A' <= c && c <= 'Z') ||
+           ('0' <= c && c <= '9') ||
+           (c == '_');
+}
+
+Token *new_token(TokenKind kind, Token *cur, char *str, int len) {
+  Token *tok = calloc(1, sizeof(Token));
+  tok->kind = kind;
+  tok->str = str;
+  tok->len = len;
+  cur->next = tok;
+  return tok;
+}
+
+// 入力文字列pをトークナイズしてそれを返す
+Token *tokenize() {
+  char *p = user_input;
+  Token head;
+  head.next = NULL;
+  Token *cur = &head;
+
+  while (*p) {
+    // 空白文字をスキップ
+    if (isspace(*p)) {
+      p++;
+      continue;
+    }
+    
+    if(startswith(p,"==") || startswith(p,"!=") ||
+       startswith(p,"<=") || startswith(p,">=")){
+      cur = new_token(TK_RESERVED, cur, p,2);
+      p += 2;
+      continue;
+    }
+
+    if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')' || *p == '>' || *p == '<' || *p == '=' || *p == ';') {
+      cur = new_token(TK_RESERVED, cur, p,1);
+      p += 1;
+      continue;
+    }
+
+    if (isdigit(*p)) {
+      cur = new_token(TK_NUM, cur, p,0);
+      char *q = p;
+      cur->val = strtol(p, &p, 10);
+      cur->len = p-q;
+      continue;
+    }
+
+    if(strncmp(p,"return",6) == 0 && !is_alnum(p[6])){
+        cur = new_token(TK_RESERVED,cur,p,6);
+        p += 6;
+        continue;
+    }
+
+    if('a' <= *p && *p <= 'z'){
+      cur = new_token(TK_IDENT,cur,p,1);
+      cur->len = 1;
+      p += 1;
+      continue;
+    }
+
+    error_at(p,"数ではありません");
+  }
+
+  new_token(TK_EOF, cur, p,0);
+  return head.next;
+}
+
+Token *consume_ident(){
+    if(token->kind != TK_IDENT)
+        return NULL;
+    Token *tok = token;
+    token = token->next;
+    return tok;
+}
